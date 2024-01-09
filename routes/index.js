bfc934778dae65165435b8d6440c4be62f2de4a5
Michal Fasanek <mfasanek@launchdarkly.com>
2024-01-09 22:29:07 +0100

    semicolons

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
