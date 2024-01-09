ec926a0d086331bd7a3c225e7edd2c9aac4ec3b1
Michal Fasanek <mfasanek@launchdarkly.com>
2024-01-09 21:55:23 +0100

    Add .env; add empty lines, update LD SDK key secret source

--- public/javascripts/utils/launchDarkly.js
+++ public/javascripts/utils/launchDarkly.js
@@ -3,7 +3,8 @@ const LaunchDarkly = require('@launchdarkly/node-server-sdk');
 let launchDarklyClient;
 
 async function initialize() {
-  const client = LaunchDarkly.init(process.env['LD_sdk_key']);
+  // const client = LaunchDarkly.init(process.env['LD_sdk_key']);
+  const client = LaunchDarkly.init(process.env.LD_SDK_KEY);
   await client.waitForInitialization();
   return client;
 }
